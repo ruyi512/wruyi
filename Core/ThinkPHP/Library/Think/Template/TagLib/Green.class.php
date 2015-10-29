7667353c7b4dc08636fdc83214e1707bfd645929
ruyitom <wry@newgamer.com>
2015-10-29 11:07:40 +0800

    create project

---
+++ Core/ThinkPHP/Library/Think/Template/TagLib/Green.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Think\Template\TagLib;
+
+//use Think\Template\TagLib;
+use Common\Util\GreenCMSTemplate;
+/**
+ * Class Green
+ * @package Think\Template\TagLib
+ */
+class Green extends GreenCMSTemplate
+{
+
+    //move to  Common\Util\GreenCMSTemplate
+}
+	
+	
